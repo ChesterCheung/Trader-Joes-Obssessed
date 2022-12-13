77409571345eb39828a87ec808f1b5e486709eae
Chester <cheung319@gmail.com>
2022-12-12 22:18:17 -0500

    Login, signup, login form created

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import LoginForm from './LoginForm'
+import SignupForm from './SignupForm'
+
+const Login = () => {
+    const [showLogin, setShowLogin] = useState("true")
+
+  return (
+    <div>
+        {showLogin ? (<LoginForm setShowLogin={setShowLogin}/>) : (<SignupForm setShowLogin={setShowLogin}/>)}   
+    </div>
+  )
+}
+
+export default Login
---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,44 @@
+import React, {useState, useContext} from 'react'
+import { UserContext } from '../context/user'
+
+const LoginForm = ({setShowLogin}) => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const {setUser} = useContext(UserContext)
+    const style ={display: "flex", justifyContent: "center", alignItems: "center"}
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetch("/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({username, password}),
+        })
+        .then(resp => resp.json())
+        .then (currentUser => {
+            setUser(currentUser)
+        })
+    }
+
+  return (
+        <div className="container">
+            <h1 style={style}>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <div style={style}>
+                    <label>Username</label>
+                    <input type="text" name="username" id="username" value={username} onChange={e => setUsername(e.target.value)}/>
+                    <label>Password</label>
+                    <input type="password" name="password" id="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                    <input style={style} type="submit" value="Login" />
+                </div>
+            </form>
+            <div style={style}>
+                <label >Don't have an account?</label>
+                <button onClick={() => setShowLogin(false)} style={style}>Sign up</button>
+            </div>
+        </div>
+  )
+}
+
+export default LoginForm
+
