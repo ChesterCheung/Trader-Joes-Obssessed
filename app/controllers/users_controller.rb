d31df197222723900c5fbc8a1387dc44a8447dd8
Chester <cheung319@gmail.com>
2022-12-12 22:18:35 -0500

    Routes for sessions created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  def index
+      users = User.all
+      render json: users
+  end
+  
+   def show
+      if current_user
+        render json: current_user, status: :ok
+      else
+        render json: "No current user set", status: :unauthorized
+      end
+    end
+  
+   def create
+      user = User.create!(user_params)
+      render json: user, status: :created
+   end
+  
+   private
+  
+    def user_params
+        params.permit(:username, :password, :password_confirmation )
+    end
+  
+    def render_unprocessable_entity_response(exception)
+        render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+    end
 end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,16 @@ Rails.application.routes.draw do
   resources :products
   resources :locations
   resources :users
+
+  ##User Routes
+  post "/signup", to: "users#create"
+  get "/staylogged", to: "users#show"
+  get "/users", to: "users#index"
+
+  # Sessions Routes
+  post "/login", to: "sessions#login"
+  delete "/logout", to: "sessions#logout"
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
