a348394c625f1c1b3d1198f8d12e7b4f7b1c7d1f
Chester <cheung319@gmail.com>
2023-01-05 00:20:40 -0500

    Removed Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,35 @@
 
-puts "Seeding"
+# puts "Seeding"
 
-user1 = User.create!(username: "Chester", password: "123", email: "cheung319@gmail.com")
-user2 = User.create!(username: "Melinda", password: "123", email: "chestercheung@outlook.com")
-user3 = User.create!(username: "Simon", password: "123", email: "cheung319@yahoo.com")
+# user1 = User.create!(username: "Chester", password: "123", email: "cheung319@gmail.com")
+# user2 = User.create!(username: "Melinda", password: "123", email: "chestercheung@outlook.com")
+# user3 = User.create!(username: "Simon", password: "123", email: "cheung319@yahoo.com")
 
-location1 = Location.create!(neighborhood: "Downtown Brooklyn", city: "Brooklyn" )
-location2 = Location.create!(neighborhood: "Union Square", city: "Manhattan" )
-location3 = Location.create!(neighborhood: "Williamsburg", city: "Brooklyn" )
-location4 = Location.create!(neighborhood: "Forrest Hills", city: "Queens" )
+# location1 = Location.create!(neighborhood: "Downtown Brooklyn", city: "Brooklyn" )
+# location2 = Location.create!(neighborhood: "Union Square", city: "Manhattan" )
+# location3 = Location.create!(neighborhood: "Williamsburg", city: "Brooklyn" )
+# location4 = Location.create!(neighborhood: "Forrest Hills", city: "Queens" )
 
-product1 = Product.create!(name: "Salsa Verde Flavored Corn Chips", category: "Snacks", price: 3, location_id: location1.id, url: "https://www.traderjoes.com/content/dam/trjo/products/m21001/73152.png/jcr:content/renditions/webp-1280.webp")
-product2 = Product.create!(name: "Onion Rings", category: "Frozen Food", price: 6, location_id: location2.id, url: "https://www.traderjoes.com/content/dam/trjo/products/m21001/72136.png/jcr:content/renditions/webp-1280.webp")
-product3 = Product.create!(name: "Almond Butter Filled Pretzel Nuggets", category: "Snacks", price: 6, location_id: location3.id, url: "https://www.traderjoes.com/content/dam/trjo/products/m21001/70721.png/jcr:content/renditions/webp-1280.webp")
-product4 = Product.create!(name: "Habanero Hot Sauce ", category: "Condiments", price: 3, location_id: location4.id, url: "https://www.traderjoes.com/content/dam/trjo/products/m20403/96159.png/jcr:content/renditions/webp-1280.webp")
-product5 = Product.create!(name: "Organic Cacio E Pepe Puffs", category: "Snacks", price: 4, location_id: location1.id, url: "https://www.traderjoes.com/content/dam/trjo/products/m21001/71323.png/jcr:content/renditions/webp-1280.webp")
+# product1 = Product.create!(name: "Salsa Verde Flavored Corn Chips", category: "Snacks", price: 3, location_id: location1.id, url: "https://www.traderjoes.com/content/dam/trjo/products/m21001/73152.png/jcr:content/renditions/webp-1280.webp")
+# product2 = Product.create!(name: "Onion Rings", category: "Frozen Food", price: 6, location_id: location2.id, url: "https://www.traderjoes.com/content/dam/trjo/products/m21001/72136.png/jcr:content/renditions/webp-1280.webp")
+# product3 = Product.create!(name: "Almond Butter Filled Pretzel Nuggets", category: "Snacks", price: 6, location_id: location3.id, url: "https://www.traderjoes.com/content/dam/trjo/products/m21001/70721.png/jcr:content/renditions/webp-1280.webp")
+# product4 = Product.create!(name: "Habanero Hot Sauce ", category: "Condiments", price: 3, location_id: location4.id, url: "https://www.traderjoes.com/content/dam/trjo/products/m20403/96159.png/jcr:content/renditions/webp-1280.webp")
+# product5 = Product.create!(name: "Organic Cacio E Pepe Puffs", category: "Snacks", price: 4, location_id: location1.id, url: "https://www.traderjoes.com/content/dam/trjo/products/m21001/71323.png/jcr:content/renditions/webp-1280.webp")
 
-review1 = Review.create!(comments: "Crazy addictive, so good", rating: 5, user_id: user1.id, product_id: product1.id)
-review2 = Review.create!(comments: "Good, but the icing is too sweet", rating: 3, user_id: user1.id, product_id: product3.id)
-review3 = Review.create!(comments: "So so good", rating: 5, user_id: user2.id, product_id: product2.id)
-review4 = Review.create!(comments: "It was so good!", rating: 4, user_id: user2.id, product_id: product3.id)
-review5 = Review.create!(comments: "Too spicy for me", rating: 1, user_id: user3.id, product_id: product4.id)
-review6 = Review.create!(comments: "Regular fish sticks", rating: 2, user_id: user1.id, product_id: product2.id)
-review6 = Review.create!(comments: "Tastes so good", rating: 4, user_id: user2.id, product_id: product5.id)
-review6 = Review.create!(comments: "meh, hot sauce is decent", rating: 3, user_id: user2.id, product_id: product4.id)
-review6 = Review.create!(comments: "It was okay", rating: 3, user_id: user3.id, product_id: product3.id)
+# review1 = Review.create!(comments: "Crazy addictive, so good", rating: 5, user_id: user1.id, product_id: product1.id)
+# review2 = Review.create!(comments: "Good, but the icing is too sweet", rating: 3, user_id: user1.id, product_id: product3.id)
+# review3 = Review.create!(comments: "So so good", rating: 5, user_id: user2.id, product_id: product2.id)
+# review4 = Review.create!(comments: "It was so good!", rating: 4, user_id: user2.id, product_id: product3.id)
+# review5 = Review.create!(comments: "Too spicy for me", rating: 1, user_id: user3.id, product_id: product4.id)
+# review6 = Review.create!(comments: "Regular fish sticks", rating: 2, user_id: user1.id, product_id: product2.id)
+# review6 = Review.create!(comments: "Tastes so good", rating: 4, user_id: user2.id, product_id: product5.id)
+# review6 = Review.create!(comments: "meh, hot sauce is decent", rating: 3, user_id: user2.id, product_id: product4.id)
+# review6 = Review.create!(comments: "It was okay", rating: 3, user_id: user3.id, product_id: product3.id)
 
-productLocation1 = ProductLocation.create!(product_id: product1.id, location_id: location1.id)
-productLocation2 = ProductLocation.create!(product_id: product2.id, location_id: location2.id)
-productLocation3 = ProductLocation.create!(product_id: product3.id, location_id: location3.id)
-productLocation4 = ProductLocation.create!(product_id: product4.id, location_id: location2.id)
-productLocation5 = ProductLocation.create!(product_id: product5.id, location_id: location4.id)
+# productLocation1 = ProductLocation.create!(product_id: product1.id, location_id: location1.id)
+# productLocation2 = ProductLocation.create!(product_id: product2.id, location_id: location2.id)
+# productLocation3 = ProductLocation.create!(product_id: product3.id, location_id: location3.id)
+# productLocation4 = ProductLocation.create!(product_id: product4.id, location_id: location2.id)
+# productLocation5 = ProductLocation.create!(product_id: product5.id, location_id: location4.id)
 
-puts "Done Seeding"
+# puts "Done Seeding"
